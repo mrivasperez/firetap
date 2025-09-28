ec653a80222411b364cd98f85cb5236f844c7afd
Miguel Rivas Perez <mrivasperez@icloud.com>
2025-09-28 13:51:10 -0700

    task: Update app set up

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,29 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+    'process.env': {},
+    'process.version': '"v18.0.0"',
+    'process.platform': '"browser"',
+  },
+  resolve: {
+    alias: {
+      stream: 'stream-browserify',
+      buffer: 'buffer',
+      events: 'events',
+      util: 'util',
+      process: 'process/browser',
+    },
+  },
+  optimizeDeps: {
+    include: [
+      'simple-peer', 
+      'buffer', 
+      'stream-browserify',
+      'events',
+      'util',
+      'process'
+    ]
+  }
 })
