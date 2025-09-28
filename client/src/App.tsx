5b1fe0cf24470e2ec9941b9083f1e9784cd77645
Miguel Rivas Perez <mrivasperez@icloud.com>
2025-09-27 20:14:28 -0700

    style: Simplify App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,15 @@
-import type { FC } from 'react'
-import './App.css'
-import Count from './components/Count'
+import type { FC } from "react";
+import "./App.css";
+import Count from "./components/Count";
 
 const App: FC = () => {
   return (
     <div id="app-root">
-      <header>
-        <h1>Realtime Tiptap — Editor</h1>
-      </header>
       <main>
-        <p>Your realtime editor will mount here.</p>
         <Count />
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
